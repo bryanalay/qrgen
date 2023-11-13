eae1aab21d64565eb48a0959e23050d8fa6df5bc
bryanalay <bryanalmi27@hotmail.com>
2023-11-12 23:09:48 -0500

    fix margin

--- src/components/QRComponent.jsx
+++ src/components/QRComponent.jsx
@@ -4,7 +4,7 @@ const QRComponent = ({qrCode,mostar}) => {
   return (
     <div className="w-full h-full sm:h-screen flex items-center justify-center bg-amber-50 ">
       {mostar && <img className='w-48 h-48' src={qrCode} alt="" />}
-      <span className="absolute bottom-0 text-gray-400">@Copyright <a className="text-black font-bold" target="_blank" rel="noreferrer" href="https://github.com/bryanalay/qrgen" > Almidev - Github</a></span>
+      <span className="absolute bottom-0 text-gray-400 m-3">@Copyright <a className="text-black font-bold" target="_blank" rel="noreferrer" href="https://github.com/bryanalay/qrgen" > Almidev - Github</a></span>
     </div>
   )
 }
